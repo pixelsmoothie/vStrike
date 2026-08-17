62d8e46771bdfaaa846f577f9df4e66dbb4bd6d0
Niraj Prabhakar Sutar <klarx.official@gmail.com>
2026-08-18 02:12:19 +0530

    Updated match timer to 90s and deployed latest build

--- docs/index.js
+++ docs/index.js
@@ -76,7 +76,7 @@ var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIR
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\Niraj\AppData\Local\Temp\tmpt2l16xtt.js
+// include: C:\Users\Niraj\AppData\Local\Temp\tmpci_rsi8p.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -212,21 +212,21 @@ Module['FS_createPath']("/assets/UI", "Modes", true, true);
 
   })();
 
-// end include: C:\Users\Niraj\AppData\Local\Temp\tmpt2l16xtt.js
-// include: C:\Users\Niraj\AppData\Local\Temp\tmphg5ygwjm.js
+// end include: C:\Users\Niraj\AppData\Local\Temp\tmpci_rsi8p.js
+// include: C:\Users\Niraj\AppData\Local\Temp\tmpgq7ulr0b.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\Niraj\AppData\Local\Temp\tmphg5ygwjm.js
-// include: C:\Users\Niraj\AppData\Local\Temp\tmpyq1qjl9t.js
+  // end include: C:\Users\Niraj\AppData\Local\Temp\tmpgq7ulr0b.js
+// include: C:\Users\Niraj\AppData\Local\Temp\tmpydg5aucd.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\Niraj\AppData\Local\Temp\tmpyq1qjl9t.js
+  // end include: C:\Users\Niraj\AppData\Local\Temp\tmpydg5aucd.js
 
 
 var programArgs = [];
